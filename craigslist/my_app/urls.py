6b41b5f2c6fa166806f4b55291398ca5e66a7cbb
Stoica Mihai <mihaivalentinstoica@gmail.com>
2020-09-06 13:42:35 +0300

    init base.html

--- craigslist/my_app/urls.py
+++ craigslist/my_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'my_app'
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('new_search', views.new_search, name='new_search')
 ]
